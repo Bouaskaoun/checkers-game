27f224a1f8ef8eaf065efac7ee69dc7073690d46
bouaskaoun <mohammed.bouaskaoun@edu.uca.ma>
2022-05-31 20:56:00 +0100

    add menu screen

--- checkers/constants.py
+++ checkers/constants.py
@@ -11,3 +11,69 @@ BLUE = (0, 0, 255)
 GREY = (128,128,128)
 
 CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'), (44, 25))
+
+"""
+This file holds the many, many constants used in display. pygame sucks.
+"""
+import pygame
+
+pygame.font.init()
+
+# fonts
+# -----------------------------------------------------------------
+TITLE_FONT = pygame.font.Font("assets/fonts/EncodeSans_black.ttf", 69)
+SUB_TITLE_FONT = pygame.font.Font("assets/fonts/UniSansBook.otf", 36)
+BODY_FONT = pygame.font.Font("assets/fonts/OpenSans_regular.ttf", 24)
+BODY_FONT_SMALL = pygame.font.Font("assets/fonts/OpenSans_regular.ttf", 16)
+BODY_FONT_EXTRA_SMALL = pygame.font.Font("assets/fonts/OpenSans_regular.ttf", 12)
+BODY_FONT_BOLD = pygame.font.Font("assets/fonts/OpenSans_bold.ttf", 24)
+
+
+# colors
+# -----------------------------------------------------------------
+PURPLE = (51, 0, 111)
+GOLD = (232, 211, 162)
+METALLIC_GOLD = (124, 123, 76)
+GRAY = (217, 217, 217)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)  # primarily for debugging/errors
+BROWN = (102, 61, 20)
+
+
+# UI
+# -----------------------------------------------------------------
+PADDING = 8  # px
+BIG_MARGIN = 48
+WIDTH_INSIDE_MARGIN = WIDTH - BIG_MARGIN * 3
+MIDDLE_X = round(WIDTH / 2)
+# 0 is standard button, 1 is smaller. 2 is menu_item button. Each is in [width, height]
+BUTTON_DIMENSIONS = [[150, 50],
+                     [100, 33],
+                     [round(WIDTH_INSIDE_MARGIN / 2) - PADDING * 2, 40],
+                     [100, 33],
+                     [80, 28]]
+BUTTON_DEFAULT_FONT = [BODY_FONT_BOLD,
+                       BODY_FONT_SMALL,
+                       BODY_FONT_SMALL,
+                       BODY_FONT_SMALL,
+                       BODY_FONT_SMALL]
+BUTTON_FONT_COLORS = [WHITE,
+                      WHITE,
+                      BLACK,
+                      BLACK,
+                      WHITE]
+INPUT_BOX_COLOR_INACTIVE = PURPLE
+INPUT_BOX_COLOR_ACTIVE = PURPLE
+INPUT_BOX_FONT = BODY_FONT_SMALL
+BOARD_X = [50, 110, 170, 230, 290, 350, 448, 508, 568, 628, 688, 748]
+BOARD_BAR_X = round((BOARD_X[5] + BOARD_X[6]) / 2)
+# note that the real board starts at index 1
+TA_BOX_DISPLAY_SIZE = 3
+STUDENT_BOX_DISPLAY_SIZE = 5
+
+
+# MISC
+# -----------------------------------------------------------------
+PLAYER_COLOR = [WHITE, RED]
+PLAYER_COLOR_STR = ["White", "Red"]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, WHITE, BLACK, TITLE_FONT, BODY_FONT_SMALL, PADDING
 from checkers.game import Game
+from checkers.button import Button
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
@@ -11,7 +12,41 @@ def get_row_col_from_mouse(pos):
     col = x // SQUARE_SIZE
     return row, col
 
-def main():
+buttons = [Button("START", "START", WIDTH / 2 + PADDING - 90, HEIGHT / 2 + PADDING * 2 + 100, BLACK, 0)]
+def drawMenu(win):
+    win.fill(WHITE)
+
+    text = TITLE_FONT.render("Backgammon", 1, BLACK)
+    win.blit(text, (round(WIDTH / 2) - round(text.get_width() / 2), 25))
+
+    text = BODY_FONT_SMALL.render("v0.1", 1, BLACK)
+    win.blit(text, (WIDTH - text.get_width() - PADDING, 150 - text.get_height() - PADDING))
+
+    for b in buttons:
+        b.draw(win)
+
+    dices = pygame.image.load("assets/backgammon_ambiance3.png")
+    win.blit(dices,(280,0))
+    pygame.display.update()
+
+def runMenu(window):
+    display_screen = "MENU"
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(60)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return "QUIT"
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                for b in buttons:
+                    if b.click(pos):
+                        if b.button_id == "START":
+                            return "INGAME"
+        drawMenu(window)
+
+def runInGame():
     run = True
     clock = pygame.time.Clock()
     game = Game(WIN)
@@ -34,6 +69,33 @@ def main():
 
         game.update()
     
+    pygame.quit()
+
+
+def main():
+    screen = runMenu(WIN)
+    run = True
+    clock = pygame.time.Clock()
+    game = Game(WIN)
+
+    while run:
+        clock.tick(60)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                for b in buttons:
+                    if b.click(pos):
+                        if b.button_id == "START":
+                            runInGame()
+
     pygame.quit()
 
 main()
